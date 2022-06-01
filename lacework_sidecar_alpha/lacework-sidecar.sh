c7bbb29c9dd2fdf5a67e3e1d2d20e6954a934ad6
Antoine <antoineb@jacurutu.ca>
2022-06-01 11:19:06 -0600

    Updated alpha release to work with 5.6 agent

--- lacework_sidecar_alpha/lacework-sidecar.sh
+++ lacework_sidecar_alpha/lacework-sidecar.sh
@@ -1,5 +1,6 @@
 #!/shared/bin/sh
-# Copyright 2014-2020 Lacework Inc.
+
+# Copyright 2014-2021 Lacework Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,22 +15,27 @@
 #     limitations under the License.
 set -e
 
+export PATH=$PATH:/shared/bin
+
 #
 # This script is meant for quick & easy sidecar install via:
-#    1. sudo sh -c /var/lib/lacework-backup/lacework-sidecar.sh -a ACCESS_TOKEN
-#    or
 #    1. export LaceworkAccessToken="ACCESS_TOKEN"
-#    2. sudo sh -c /var/lib/lacework-backup/lacework-sidecar.sh
-#
+#    2. sh -c /var/lib/lacework-backup/lacework-sidecar.sh
+#    Note: SERVER_URL is the Lacework Server URL specific to your region.
+#          The environment variable LaceworkServerUrl will set this value.
+#          If not provided, the US URL will be assumed.
+#    Note: This script can also be used as an ENTRYPOINT script. It will exec
+#          any arguments (aside from a legacy access token and -U SERVER_URL).
+# For debugging set the environment variable LaceworkVerbose:
+#    export LaceworkVerbose=true
 
-STRICT_MODE=no
 # Agent version
-version=3.6.3
-dc_sha1=cca2d5b331b1670d4a482a743c5619ef157ea698
-dc_musl_sha1=1eb35425998fb5ab0f32aa1771d22f37ef81f903
-dc_arm64_sha1=
+VERSION=5.6.0.8352
 
-ARG1=${1}
+TOKEN=""
+SERVER_URL=""
+#default server url
+lw_server_url="https://api.lacework.net"
 
 # extra protection for mktemp: when it fails - returns fallback value
 mktemp_safe() {
@@ -43,61 +49,8 @@ mktemp_safe() {
 	fi
 }
 
-check_bash() {
-	if [ "$ARG1" = "" ];
-	then
-		if [ "$0" = "bash" ] ||  [ "$0" = "sh" ];
-		then
-			cat <<-EOF
-			----------------------------------
-			Error:
-			This scripts needs user input and is unable to read the input. 
-			Please run 1 of the following ways
-
-			1. sudo sh -c "\$(curl -sSL ${download_url}/install.sh)" 
-
-			OR a 2 step process to download file to /tmp and run it from there.
-
-			1. "curl -sSL ${download_url}/install.sh > /tmp/install.sh"
-			2. sudo sh /tmp/install.sh
-			----------------------------------
-			EOF
-			exit 100
-		fi
-	fi
-}
-
 command_exists() {
 	command -v "$@" > /dev/null 2>&1
-}
-
-# Check if this is a forked Linux distro
-check_forked() {
-	# Check for lsb_release command existence, it usually exists in forked distros
-	if command_exists lsb_release; then
-		# Check if the `-u` option is supported
-		set +e
-		lsb_release -a -u > /dev/null 2>&1
-		lsb_release_exit_code=$?
-		set -e
-
-		# Check if the command has exited successfully, it means we're in a forked distro
-		if [ "$lsb_release_exit_code" = "0" ]; then
-			# Print info about current distro
-			cat <<-EOF
-			You're using '$lsb_dist' version '$dist_version'.
-			EOF
-
-			# Get the upstream release info
-			lsb_dist=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'id' | cut -d ':' -f 2 | tr -d '[[:space:]]')
-			dist_version=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[[:space:]]')
-
-			# Print info about upstream distro
-			cat <<-EOF
-			Upstream release is '$lsb_dist' version '$dist_version'.
-			EOF
-		fi
-	fi
 }
 
 check_x64() {
@@ -105,20 +58,15 @@ check_x64() {
 		*64)
 			;;
 		*)
-			cat >&2 <<-'EOF'
-			----------------------------------
-			Error: you are using a 32 bit kernel.
-			Lacework currently only supports 64bit platforms.
-			----------------------------------
-			EOF
+			echo "ERROR: Lacework currently only supports 64bit platforms"
 			exit 200
 			;;
 	esac
 }
 
 check_root_cert() {
-
-	echo "Check Go Daddy root certificate"
+	set +e
+	echo "INFO: Checking for Go Daddy root certificate"
 	GODADDY_ROOT_CERT=$(mktemp_safe .cert /tmp/godaddy.cert)
 	LW_INSTALLER_KEY=$(mktemp_safe .cert /tmp/installer_key.cert)
 	cat > ${GODADDY_ROOT_CERT} <<-'EOF'
@@ -158,48 +106,60 @@ check_root_cert() {
 			if [ -z "${cert_path}" ]; then
 				cert_path="/etc/ssl"
 			fi
-			cert_ok=`openssl verify -x509_strict ${GODADDY_ROOT_CERT}`
+			cert_ok=`openssl verify -x509_strict ${GODADDY_ROOT_CERT} 2>&1`
 			if [ ! -z "${cert_ok##*$reqsubstr*}" ];	then
 				openssl x509 -noout -in ${GODADDY_ROOT_CERT} -pubkey > ${LW_INSTALLER_KEY}
 				cert_ok=`awk -v cmd='openssl x509 -noout -pubkey | cmp -s ${LW_INSTALLER_KEY}; if [ $? -eq 0 ]; then echo "installed"; fi' '/BEGIN/{close(cmd)};{print | cmd}' < ${cert_path}/certs/ca-certificates.crt`
 				if [ "${cert_ok}" != "installed" ]; then
-					cat >&2 <<-'EOF'
-					----------------------------------
-					Error: this installer requires Go Daddy root certificate to be installed
-					Please ensure the root certificate is installed and retry.
-					----------------------------------
-					EOF
-					if [ "${STRICT_MODE}" = "yes" ]; then
-						rm -f ${GODADDY_ROOT_CERT}
-						rm -f ${LW_INSTALLER_KEY}
-						exit 300
-					fi
+					echo "WARNING: Could not find Go Daddy root certificate"
 				fi
 			fi
 		fi
 	fi
 	rm -f ${GODADDY_ROOT_CERT}
 	rm -f ${LW_INSTALLER_KEY}
+	set -e
+}
+
+get_serverurl_from_cfg_file() {
+	if command_exists awk; then
+		if [ -f /var/lib/lacework/config/config.json ]; then
+			config_url=$(grep -v "#" /var/lib/lacework/config/config.json)
+			config_url=$(echo $config_url | awk 'BEGIN {RS=","} match($0, /serverurl([^,]+)/) { print substr( $0, RSTART, RLENGTH )}')
+			config_url=$(echo $config_url | awk 'BEGIN{ORS=""}{print $0}')
+			config_url=$(echo $config_url | sed 's/[\} ]//g')
+			config_url=$(echo $config_url | awk 'BEGIN {FS="\""} {print $3}')
+			if [ ! -z "${config_url}" ]; then
+				echo "${config_url}"
+				return
+			fi
+		fi
+	fi
+	echo ""
+}
+
+read_lw_server_url() {
+	cfg_url=$(get_serverurl_from_cfg_file)
+	if [ ! -z "${cfg_url}" ]; then
+		echo "INFO: Using serverurl already set in local config: ${cfg_url}"
+		lw_server_url=${cfg_url}
+		return
+	fi
+	if [ ! -z "$SERVER_URL" ];
+	then
+		lw_server_url=$SERVER_URL
+	fi
 }
 
 check_lw_connectivity() {
-	lw_cfg_url="https://api.lacework.net/upgrade/?name=datacollector&version=${version}"
-
-	if [ "${STRICT_MODE}" = "no" ]; then
-		set +e
+	lw_cfg_url=$(get_serverurl_from_cfg_file)
+	if [ -z "${lw_cfg_url}" ]; then
+		lw_cfg_url="${lw_server_url}/upgrade/?name=datacollector&version=${VERSION}"
 	fi
-	echo "Check connectivity to Lacework server"
+
+	set +e
+	echo "INFO: Check connectivity to Lacework server"
 	if command_exists awk; then
-		if [ -f /var/lib/lacework/config/config.json ]
-		then
-			config_url=`grep -v "#" /var/lib/lacework/config/config.json | awk 'match($0, /serverurl([^,]+)/) { print substr( $0, RSTART, RLENGTH )}'`
-			if [ ! -z "${config_url}" ]; then
-				config_url=`echo $config_url | sed 's/"//g' | awk -F: '{print $2":"$3}'`
-				if [ ! -z "${config_url}" ]; then
-					lw_cfg_url="${config_url}"
-				fi
-			fi
-		fi
 		if command_exists curl; then
 			response=`curl -o /dev/null -w "%{http_code}" -sSL ${lw_cfg_url}`
 		elif command_exists wget; then
@@ -210,52 +170,28 @@ check_lw_connectivity() {
 			if [ $? == 0 ]; then
 				response="200"
 			fi
+		else
+			echo "WARNING: Connectivity to ${lw_cfg_url} could not be verified"
+			# Fake the response
+			response="200"
 		fi
 		if [ "${response}" != "200" ]; then
-			cat >&2 <<-EOF
-			----------------------------------
-			Error: this installer needs the ability to contact $lw_cfg_url
-			Please ensure this machine is able to connect to the network
-			and/or requires correct proxy settings
-			----------------------------------
-			EOF
-			if [ "${STRICT_MODE}" = "yes" ]; then
-				exit 400
-			fi
+			echo "WARNING: Unexpected response code from ${lw_cfg_url}: ${response}"
 		fi
 	fi
-	if [ "${STRICT_MODE}" = "no" ]; then
-		set -e
-	fi
+	set -e
 }
 
-shell_prefix() {
+check_root() {
 	user=$(whoami)
 	if [ "$user" != 'root' ]; then
-		cat >&2 <<-'EOF'
-		----------------------------------
-		Error: this installer needs the ability to run commands as root.
-		Please run as root or with sudo
-		----------------------------------
-		EOF
+		echo "ERROR: Please run as the root user"
 		exit 500
 	fi
 }
 
-get_curl() {
-	if command_exists curl; then
-		curl='curl -sSL'
-	elif command_exists wget; then
-		curl='wget -qO-'
-	elif command_exists busybox && busybox --list-modules | grep -q wget; then
-		curl='busybox wget -qO-'
-	fi
-}
-
 get_lsb_dist() {
-
 	# perform some very rudimentary platform detection
-
 	if [ -z "$lsb_dist" ] && command_exists lsb_release; then
 		lsb_dist="$(lsb_release -si)"
 	fi
@@ -287,28 +223,29 @@ get_lsb_dist() {
 	lsb_dist="$(echo "$lsb_dist" | tr '[:upper:]' '[:lower:]')"
 }
 
-check_user_x64() {
-	case "$lsb_dist" in
-		*ubuntu*|*debian*)
-			case "$(dpkg --print-architecture)" in
-				*64)
-					;;
-				*)
-					cat >&2 <<-'EOF'
-					----------------------------------
-					Error: Package manager (dpkg) does not support 64bit binaries.
-					Lacework currently only supports 64bit platforms.
-					----------------------------------
-					EOF
-					exit 600
-					;;
-			esac
-			;;
-		*fedora*|*centos*|*redhatenterprise*|*oracleserver*|*scientific*)
-			;;
-		*)
-			;;
-	esac
+# Check if this is a forked Linux distro
+check_forked() {
+	# Check for lsb_release command existence, it usually exists in forked distros
+	if command_exists lsb_release; then
+		# Check if the `-u` option is supported
+		set +e
+		lsb_release -a -u > /dev/null 2>&1
+		lsb_release_exit_code=$?
+		set -e
+
+		# Check if the command has exited successfully, it means we're in a forked distro
+		if [ "$lsb_release_exit_code" = "0" ]; then
+			# Print info about current distro
+			echo "INFO: Using '${lsb_dist}' version '${dist_version}'"
+
+			# Get the upstream release info
+			lsb_dist=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'id' | cut -d ':' -f 2 | tr -d '[[:space:]]')
+			dist_version=$(lsb_release -a -u 2>&1 | tr '[:upper:]' '[:lower:]' | grep -E 'codename' | cut -d ':' -f 2 | tr -d '[[:space:]]')
+
+			# Print info about upstream distro
+			echo "INFO: Upstream release is '${lsb_dist}' version '${dist_version}'"
+		fi
+	fi
 }
 
 get_dist_version() {
@@ -352,27 +289,17 @@ get_dist_version() {
 }
 
 die() {
-	echo "error:" "$@" >&2
+	echo "ERROR: " "$@" >&2
 	exit 1
 }
 
 setup_paths() {
-	DisplayVer=$version
-	DCSUFFIX=-musl.gz
-	file_sha1=$(sha1sum /var/lib/lacework-backup/${DisplayVer}/datacollector$DCSUFFIX | cut -d " " -f 1)
-	exp_sha1=${dc_musl_sha1}
+	DisplayVer=$VERSION
+	DCSUFFIX=-musl
 
-        cp -r /shared/lib/* /lib
-
-	if [ "${exp_sha1}" != "${file_sha1}" ]; then
-		echo "----------------------------------"
-		echo "Download sha1 checksum failed, [${exp_sha1}] [${file_sha1}]"
-		echo "----------------------------------"
-		exit 700
-	fi
 	if [ ! -d "/var/lib/lacework/${DisplayVer}" ]; then
 		mkdir -p "/var/lib/lacework/${DisplayVer}" || die "mkdir failed : /var/lib/lacework"
-		gunzip -c /var/lib/lacework-backup/${DisplayVer}/datacollector${DCSUFFIX} > "/var/lib/lacework/${DisplayVer}/datacollector" || die "cp failed : /var/lib/lacework"
+		cp /var/lib/lacework-backup/${DisplayVer}/datacollector${DCSUFFIX} "/var/lib/lacework/${DisplayVer}/datacollector" || die "cp failed : /var/lib/lacework"
 		chmod +x /var/lib/lacework/${DisplayVer}/datacollector
 	fi
 	if [ ! -f "/var/lib/lacework/datacollector" ]; then
@@ -384,112 +311,129 @@ setup_paths() {
 	if [ ! -d "/var/log/lacework" ]; then
 		mkdir -p "/var/log/lacework" || die "mkdir failed : /var/log/lacework"
 	fi
-	chown -R root:root /var/lib/lacework
-	chown -R root:root /var/log/lacework
+	# For k8s, the config directory is read only (configmap). Failing to change
+	# the permissions prevents agent from starting. So ignore errors if any
+	chown -R root:root /var/lib/lacework || true
+	chown -R root:root /var/log/lacework || true
 }
 
 # Customized parameters
-get_config() {
-
-	if [ ! -f /var/lib/lacework/config/config.json ]
-	then
-		if [ "$ARG1" = "" ];
-		then
-			read -p "Please enter access token: " access_token
-		else
-			access_token=$ARG1
-		fi
-		if [ "$access_token" = "" ];
-		then
-			echo "Not a valid access_token"
-			exit 800
-		fi
-		rbacTokenLen="1-30"
-		LwTokenShort=`echo "$access_token" |cut -c${rbacTokenLen}`
-		echo "Using access token : $LwTokenShort ..."
-		echo "Writing configuration file"
+write_config() {
+	if [ ! -f /var/lib/lacework/config/config.json ]; then
+		rbacTokenLen="1-8"
+		LwTokenShort=`echo "${TOKEN}" |cut -c${rbacTokenLen}`
+		echo "INFO: Using access token: ${LwTokenShort}*** ..."
+		echo "INFO: Using server url: $lw_server_url"
+		echo "INFO: Writing configuration file"
 
 		(set -x; $sh_c 'mkdir -p /var/lib/lacework/config')
 		($sh_c 'echo "+ sh -c Writing config.json in /var/lib/lacework/config"')
 		($sh_c "echo \"{\" > /var/lib/lacework/config/config.json")
-		($sh_c "echo \" \\\"tokens\\\" : { \\\"AccessToken\\\" : \\\"${access_token}\\\" } \"    >> /var/lib/lacework/config/config.json")
+		($sh_c "echo \" \\\"tokens\\\" : { \\\"AccessToken\\\" : \\\"${TOKEN}\\\" } \"    >> /var/lib/lacework/config/config.json")
+		($sh_c "echo \" ,\\\"serverurl\\\" : \\\"${lw_server_url}\\\" \"    >> /var/lib/lacework/config/config.json")
 		($sh_c "echo \"}\" >> /var/lib/lacework/config/config.json")
 	else
-		echo "Skipping writing config since a config file already exists"
+		echo "INFO: Skipping writing config since a config file already exists"
 	fi
 }
 
+# set permission of file
+set_file_permissions() {
+	set +e
+	if [ -f /var/lib/lacework/config/config.json ]; then
+	   chmod 640 /var/lib/lacework/config/config.json
+	fi
+	set -e
+}
 
 do_install() {
-	check_bash
 	check_x64
 
 	sh_c='sh -c'
-	shell_prefix
+	check_root
 
 	lsb_dist=''
 	get_lsb_dist
 
-	check_lw_connectivity
-
-	check_root_cert
-
-	#check_user_x64
-
 	dist_version=''
 	get_dist_version
+
+	read_lw_server_url
+	check_lw_connectivity
+	check_root_cert
 
 	# Check if this is a forked Linux distro
 	check_forked
 
-	echo "Installing on  $lsb_dist ($dist_version)"
+	echo "INFO: Installing on $lsb_dist ($dist_version)"
 
-	get_config
+	write_config
+
+	# Set config.json's permission to 0640
+	set_file_permissions
+
 	setup_paths
-	if [ -z "${ARG1}" ]; then
-		ARG1="InvalidToken"
-	fi
-	# run the binary now
-	if [ ! -f /var/lib/lacework/config/config.json ]
-	then
-		/var/lib/lacework/datacollector -a ${ARG1} &
-	else
-		/var/lib/lacework/datacollector &
-	fi 
 
-	echo "Lacework successfully installed and Launched"
+	cp /shared/lib/* /lib
+
+	/var/lib/lacework/datacollector &
+
+	if [ ! -z "${LaceworkVerbose}" ]; then
+		touch /var/log/lacework/datacollector.log
+		tail -f /var/log/lacework/datacollector.log &
+	fi
+
+	echo "INFO: Lacework successfully installed and Launched"
 }
 
-# wrapped up in a function so that we have some protection against only getting
-# half the file during "curl | sh"
-while getopts "SOh" arg; do
-	case $arg in
-		h)
-			cat >&2 <<-'EOF'
-			----------------------------------
-			Usage: sudo install.sh -h [-S] [-O]
-			-h: usage banner
-			[Optional Parameters]
-			-S: enable strict mode
-			----------------------------------
-			EOF
-			exit 0
-			;;
-		S)
-			STRICT_MODE=yes
-			shift
-			;;
-	esac
-done
-
-if [ ! -z "${ARG1}" ]; then
-	ARG1=`echo ${ARG1} | grep -E '^[[:alnum:]][-[:alnum:]]{0,55}[[:alnum:]]$'`
-elif [ ! -z "${LaceworkAccessToken}" ]; then
-	ARG1=`echo ${LaceworkAccessToken} | grep -E '^[[:alnum:]][-[:alnum:]]{0,55}[[:alnum:]]$'`
+# Debug output and helpful environment settings.
+if [ ! -z "${LaceworkVerbose}" ]; then
+	set -x
+	whoami
+	if [ -f /etc/os-release ]; then
+		cat /etc/os-release;
+	fi
 fi
-echo "Begin Install $ARG1 $LaceworkAccessToken"
+
+# First try to find the access token via an environment variable.
+if [ ! -z "${LaceworkAccessToken}" ]; then
+	TOKEN=`echo ${LaceworkAccessToken} | grep -E '^[A-Za-z0-9][-_A-Za-z0-9]{10,55}[A-Za-z0-9]$' || true`
+else
+	echo "INFO: Access token not provided in LaceworkAccessToken environment variable"
+fi
+
+# Check if access token is passed as the first parameter
+if [ -z "${TOKEN}" ]; then
+	if [ "$#" -gt 0 ] && [ "${1}" = "${1#-}" ]; then
+		TOKEN=`echo ${1} | grep -E '^[A-Za-z0-9][-_A-Za-z0-9]{10,55}[A-Za-z0-9]$' || true`
+		if [ ! -z "${TOKEN}" ]; then
+			# The first argument parsed as an access token.
+			echo "INFO: Access token provided via commandline arguments"
+			shift
+		fi
+	fi
+fi
+
+if [ ! -z "${LaceworkServerUrl}" ]; then
+	SERVER_URL="${LaceworkServerUrl}"
+fi
+
+if [ "$#" -gt 1 ] && [ "${1}" = "-U" ]; then
+	echo "INFO: Server url provided via commandline arguments"
+	SERVER_URL=${2}
+	shift
+	shift
+fi
+
+# Require a token to be provided by an environment variable, parameter, or configuration.
+if [ -z "${TOKEN}" ] && [ ! -f /var/lib/lacework/config/config.json ]; then
+	echo "ERROR: Cannot find Lacework access token"
+	exit 100
+fi
+
 do_install
 
 echo "Docker RUN_CMD = $RUN_CMD"
 $RUN_CMD
+
 exit 0
